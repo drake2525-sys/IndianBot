e633e209938c2d8d6afaca50092cc5d30b7b3507
drake2525-sys <67995194+drake2525-sys@users.noreply.github.com>
2020-07-09 14:36:24 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,14 +8,11 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-INDIANBOT_IS_ALIVE = Jinda Hu Be!
+ Jinda Hu Be!
                      
 
 
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
-    chat = await alive.get_chat()
-    await alive.delete()
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_message(chat, INDIANBOT_IS_ALIVE) 
+    
